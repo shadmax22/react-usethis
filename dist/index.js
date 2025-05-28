c983cbc3105bfd0eb2ace11baf904130c0e58337
Md Shad <mdshad.a@branchx.in>
2025-05-28 13:08:39 +0530

    Bump version to 0.1.685 and update useThis instance version to 0.1.684

--- dist/index.js
+++ dist/index.js
@@ -209,7 +209,7 @@ function M(e) {
       throw Error("Cannot use useThis Instance without created");
     return F(t.stateName);
   };
-  return n["@___usethis"] = "0.1.682", n.this = t.stateName, n.onEffect = (s, r) => (t.onEffect = {
+  return n["@___usethis"] = "0.1.684", n.this = t.stateName, n.onEffect = (s, r) => (t.onEffect = {
     resolver: s,
     dependent_states: r
   }, n), n.stateName = (s) => (t.stateName = s, n), n.default = (s) => (t.defaultData = s, n), n.setConfig = (s, r) => (t[s] = r, n), n.use = {}, n.create = () => {
